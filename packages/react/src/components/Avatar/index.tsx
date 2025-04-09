6704bc5b7d991318113cdc41ff0302ebc0e0672d
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-04-09 12:03:47 -0300

    feat: design system version 1

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,19 @@
+import { User } from '@phosphor-icons/react'
+import React, { ComponentProps } from 'react'
+import { AvatarContainer, AvatarImage, AvatarFallback } from './styles'
+
+export type AvatarProps = ComponentProps<typeof AvatarImage>
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
+
+Avatar.displayName = 'Avatar'
