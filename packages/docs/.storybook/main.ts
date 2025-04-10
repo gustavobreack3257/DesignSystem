d15c16ee5232c43cad31e98e4f80d83fa53c3912
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-04-10 11:31:52 -0300

    feat: Publish and configuration with storybook ajusted

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,31 +1,28 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
-import { join, dirname } from "path";
-
+import { join, dirname } from "path"
 
 /**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
-  stories: ["../src/pages/**/*.mdx" ,"../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
-  addons: [
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/experimental-addon-test"),
-    getAbsolutePath("@storybook/addon-a11y")
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
-    options: {},
-  },
-  docs: {
-    autodocs: true,
-    defaultName: 'Docs'
-  },
+  "addons": [
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath("@storybook/experimental-addon-test")
+  ],
+  "framework": {
+    "name": getAbsolutePath('@storybook/react-vite'),
+    "options": {}
+  }
 };
 export default config;
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj, StoryContext } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from '../../../react/src/index'
 import { ArrowRight } from '@phosphor-icons/react'
 
