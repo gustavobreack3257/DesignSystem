1e9d3dc3e962af2e0787fe17c2f4ddc48ef339c4
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2025-04-11 08:36:59 -0300

    fix: deploy-docs

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -5,8 +5,8 @@ export default {
   title: 'Data display/Avatar',
   component: Avatar,
   args: {
-    src: 'https://github.com/diego3g.png',
-    alt: 'Diego Fernandes',
+    src: 'https://github.com/gustavobreack3257.png',
+    alt: 'Gustavo Costa',
   },
   argTypes: {
     src: {
